5cbb1944ade0a2334189298b54e4a18de92f3404
Felipe Patricio Albornoz <felipe_albornoz@estudante.sesisenai.org.br>
2025-11-10 22:48:05 -0300

    parte4: criarUsuario(nome)

--- exercicios/Ex4.js
+++ exercicios/Ex4.js
@@ -18,3 +18,21 @@ async function executarLeitura() {
 async function escreverDB(data) {
     await fs.writeFile(dbPath, JSON.stringify(data, null, 2));
 }
+// Para usar, so escrever na terminal: node Ex4.js criar Ana e depois node Ex4.js listar
+async function criarUsuario(nome) {
+    const db = await readDB();
+    const novoId = db.usuarios.length > 0 
+        ? Math.max(...db.usuarios.map(u => u.id)) + 1 
+        : 1;
+
+    const novoUsuario = { id: novoId, nome };
+    db.usuarios.push(novoUsuario);
+
+    await writeDB(db);
+    console.log('Usuario criado:', novoUsuario);
+}
+
+async function listarUsuarios() {
+    const db = await readDB();
+    console.log('Lista de usuarios:', db.usuarios);
+}
